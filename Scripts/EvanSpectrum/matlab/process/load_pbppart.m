e8ce3cb05ca2f0001bdb159c8d39f01b9a98a301
Evan Weinberg <weinbe2@bu.edu>
2015-07-15 01:51:34 -0400

    Improved loading correlators by replacing for loops with reshape. Still need to optimize file loading. Issue #8.

--- Scripts/EvanSpectrum/matlab/process/load_pbppart.m
+++ Scripts/EvanSpectrum/matlab/process/load_pbppart.m
@@ -2,24 +2,27 @@
 function [correlator varargout] = load_pbppart(fname, parse_Nt, parse_Ns, number_bl)
 	% Number of extra arguments.
 	nout = max(nargout,1) - 1;
-	
+
 	% Business as usual.
 	full_fname = strcat(fname, '/spectrum2/stoch/PBPPART.dat');
 	fd = fopen(full_fname,'rt');
 	raw_data = textscan(fd, '%d%d%d%f%f', 'Whitespace', ' \t');
-	fclose(fd);
+	fclose(fd);    
+    
 	raw_data{1} = double(raw_data{1});
 	raw_data{2} = double(raw_data{2});
 	raw_data{3} = double(raw_data{3});
 	raw_data = cell2mat(raw_data);
     %raw_data = importdata(full_fname);
 	num_data = floor(size(raw_data, 1)/(parse_Nt*number_bl));
-    correlator = zeros(parse_Nt, number_bl, num_data);
+    %correlator = zeros(parse_Nt, number_bl, num_data);
 	
 	if (nout == 1)
 		varargout{1} = zeros(1, num_data);
-	end
+    end
 	
+    % Old! The reshape method is faster.
+    %{
 	for i=1:num_data
         for j=1:number_bl
             for k=1:parse_Nt
@@ -30,4 +33,16 @@ function [correlator varargout] = load_pbppart(fname, parse_Nt, parse_Ns, number
             end
         end    
     end
+    %}
+	
+
+	correlator = reshape(raw_data(:,5), [parse_Nt, number_bl, num_data])*sqrt((parse_Ns)^3*parse_Nt);
+    if (nout == 1)
+        tmpinfo = reshape(raw_data(:,1), [parse_Nt, number_bl, num_data]);
+        varargout{1}(1,:) = tmpinfo(1,1,:);
+        clear('tmpinfo');
+    end
+        
+    
 end
+
